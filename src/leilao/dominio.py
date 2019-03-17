4218923d49883ea0b3b486ea8cf973f94f713c91
silrait <thiarllisba@hotmail.com>
2019-03-17 02:59:34 -0300

    Primeiro Bug para tratar

--- src/leilao/dominio.py
+++ src/leilao/dominio.py
@@ -1,3 +1,6 @@
+import sys
+
+
 class Usuario:
 
     def __init__(self, nome):
@@ -7,12 +10,21 @@ class Usuario:
     def nome(self):
         return self.__nome
 
+    def __str__(self):
+        return f'Usuario [nome = {self.__nome}]'
+
 
 class Lance:
 
     def __init__(self, usuario, valor):
         self.usuario = usuario
         self.valor = valor
+
+    def __str__(self):
+        return f'Lance [usuario = {self.usuario}, valor = {self.valor}]'
+
+    def __repr__(self):
+        return self.__str__()
 
 
 class Leilao:
@@ -24,3 +36,38 @@ class Leilao:
     @property
     def lances(self):
         return self.__lances
+
+    def __str__(self):
+        return f'Leilao [descricao = {self.descricao}, Lances = {self.lances}]'
+
+    def __getitem__(self, item):
+        return self.__lances[item]
+
+    def __len__(self):
+        return len(self.__lances)
+
+
+class Avaliador:
+    def __init__(self):
+        self.__maior_lance = sys.float_info.min
+        self.__menor_lance = sys.float_info.max
+
+    @property
+    def maior_lance(self):
+        return self.__maior_lance
+
+    @property
+    def menor_lance(self):
+        return self.__menor_lance
+
+    def avalia(self, leilao: Leilao):
+        for lance in leilao.lances:
+            if(lance.valor > self.maior_lance):
+                self.__maior_lance = lance.valor
+            elif(lance.valor < self.menor_lance):
+                self.__menor_lance = lance.valor
+
+
+    def __str__(self):
+        return f'Avaliador [maior lance = {self.maior_lance}, menor lance = {self.menor_lance}]'
+
---
+++ src/leilao/principal.py
@@ -0,0 +1,18 @@
+from src.leilao.dominio import Usuario, Lance, Leilao, Avaliador
+
+gui = Usuario('Gui')
+yuri = Usuario('Yuri')
+
+lance_do_yuri = Lance(yuri, 100.0)
+lance_do_gui = Lance(gui, 150.0)
+
+leilao = Leilao('Celular')
+
+leilao.lances.append(lance_do_yuri)
+leilao.lances.append(lance_do_gui)
+
+print(leilao)
+
+avaliador = Avaliador()
+avaliador.avalia(leilao)
+print(avaliador)
